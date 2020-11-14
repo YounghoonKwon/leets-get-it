f4e559699601576c7a7c5221083f1b8953dacf3a
younghoonkwon <younghoon.kwon91@gmail.com>
2020-11-14 19:20:20 +0900

    Create younghoon.py

---
+++ 201117-Leet4-MedianOfTwoSortedArrays/younghoon.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums3 = nums1 + nums2
+        nums3.sort()
+        if len(nums3) %2 == 1:
+            return float(nums3[len(nums3)//2])
+        else:
+            return float(nums3[len(nums3)//2] + nums3[len(nums3)//2-1])/2
