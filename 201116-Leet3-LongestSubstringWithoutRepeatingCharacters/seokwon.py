d55cb1bde64f4e885b19b66835e7ae320cf9f8ae
seok10code <73082542+seok10code@users.noreply.github.com>
2020-11-16 13:35:27 +0900

    Update seokwon.py

--- 201116-Leet3-LongestSubstringWithoutRepeatingCharacters/seokwon.py
+++ 201116-Leet3-LongestSubstringWithoutRepeatingCharacters/seokwon.py
@@ -2,10 +2,9 @@ class Solution(object):
     def lengthOfLongestSubstring(self, s):
         result = ''
         maxLength = 0
-        for idx, char in enumerate(s):
+        for char in s:
             if char not in result:
                 result += char
-                print('result: ', result)
             else:
                 result = result[result.find(char)+1:]+char
             maxLength = max(maxLength, len(result))
