579e6a4db19f1f8b3bb082f5cb1c2368b5fadfa4
seok10code <73082542+seok10code@users.noreply.github.com>
2020-11-16 10:39:09 +0900

    Create seokwon.py

---
+++ 201116-Leet2-AddTwoNumbers/seokwon.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        twosum = 0
+        exp = 0
+        ten = 10
+        while l1:
+            twosum += (ten**exp)*l1.val
+            l1 = l1.next
+            exp +=1
+        else:
+            exp =0
+            
+        while l2:
+            twosum += (ten**exp)*l2.val
+            l2 = l2.next
+            exp +=1
+        return makingReturnNode(twosum)
+        
+
+def makingReturnNode(inputValue : int):
+    eachNum = convert_Int_To_List(inputValue)
+    result = None
+    for i in eachNum:
+        Node = ListNode(i,result)
+        result = Node
+    return result
+    
+def convert_Int_To_List(inte : int) ->list:
+    res = [int(x) for x in str(inte)]
+    return res
