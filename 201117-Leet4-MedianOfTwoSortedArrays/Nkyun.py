009827434f032790d6341de527eb6a1bcb54da92
Namkyun Kim <55725244+Namkyun-AUX@users.noreply.github.com>
2020-11-17 23:48:57 +0900

    Create Nkyun.py

---
+++ 201117-Leet4-MedianOfTwoSortedArrays/Nkyun.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        nums = sorted(nums1 + nums2)  
+        if len(nums) % 2 == 0:
+            return (nums[len(nums) // 2] + nums[len(nums) // 2 - 1]) / 2
+        else:
+            return nums[len(nums) // 2]
