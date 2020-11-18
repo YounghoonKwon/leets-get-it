1650bcaf3c2a7cd85722c527595ccf35e0a1bdf0
wow-woo <51289355+wow-woo@users.noreply.github.com>
2020-11-18 17:23:34 +0900

    Shrink scope with min height pointer
    - Fill = minHeight * indexDistance
    - Move inward if minHeight is smaller than next
    - Keep update on maxFill

--- 201120-Leet11-ContainerWithMostWater/wow-woo.py
+++ 201120-Leet11-ContainerWithMostWater/wow-woo.py
@@ -1,21 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        #edge case
-        if len(height) == 2:
-            return min(height)
-        dp={}
+        vol = 0
         
-        for i in range(len(height)-1):
-            for j in range(i+1, len(height)):
-                dis = abs(i-j)
-                #Avoiding duplicates
-                current = min(height[i], height[j])
-                if dis in dp:
-                    dp[dis] = max(dp[dis], current)
-                else:
-                    dp[dis] = current
-        result = 0
-        for item in dp:
-            current = item * dp[item]
-            result = max(result, current)
-        return result
+        i, j = 0, len(height) - 1
+        while i < j:
+            vol = max(vol, (j - i) * min(height[i], height[j]))
+            i1, j1 = i, j
+            if height[i] <= height[j]:
+                i1 += 1
+                while i1 < j and height[i1] <= height[i]:
+                    i1 += 1
+            if height[i] >= height[j]:
+                j1 -= 1
+                while j1 > i1 and height[j1] <= height[j]:
+                    j1 -= 1
+            
+            i, j = i1, j1
+        
+        return vol
