f61f1a6d369d484bf0d2b621f1c15e9a43fc6bf5
wow-woo <51289355+wow-woo@users.noreply.github.com>
2020-11-19 19:14:02 +0900

    Solution01

--- 201119-Leet10-RegularExpressionMatching/wow-woo.py
+++ 201119-Leet10-RegularExpressionMatching/wow-woo.py
@@ -1,46 +1,51 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        p_idx = 0
-        for i in range(len(s)):
-            current = s[i]
-            print('current', s[i], i)
-            
-            p_idx = self.rcs(current, p_idx, p)
-            print('ss', p_idx)
-            if i == len(s)-1 and p_idx >= len(s):
+        # Edge case
+        if p is '':
+            if s is '':
                 return True
-            if p_idx < 0 or p_idx >= len(p):
-                return False
-        return True
-    
-    def rcs(self, current, p_idx, p):
-        if p_idx >= len(p):
-            return -1
-        
-        pattern = p[p_idx]
-        print('pattern', pattern, p_idx)
-        # there is .
-        if pattern == '.':
-            return p_idx + 1
-        # there is *
-        elif pattern == '*':
-            # extend prior chractor with *
-            case1 = self.rcs(current, p_idx-1, p)
-            case2 = self.rcs(current, p_idx + 1, p)
-            return case1, case2
-        # there is only a letter
-        else:
-            # match
-            if current == pattern:
-                return p_idx+1
             else:
-                # case of skip a charactor with * #compare current with charactor post *
-                if '*' == p[p_idx+1]:
-                    print('start to check whether skipping or wrong')
-                    print(p[p_idx], p[p_idx+1])
-                    print('result', self.rcs(current, p_idx+2, p))
-                    return self.rcs(current, p_idx+2, p)
-                else:
-                    # in order to return False
-                    return -1
-        
+                return False
+        # Simplified version:
+        # if not p:
+        #     return not s
+
+        if s:
+            first_char_match = p[0] in (s[0], '.')
+        else:
+            first_char_match = False
+        # Simplified version:
+        # first_char_match = bool(s) and p[0] in (s[0], '.')
+
+        # len(p) < 2, p[1] will raise error
+        if len(p) > 1 and p[1] == '*':
+            """
+            Try all possibilities:
+            1) 0 occurrence, zero_ocr
+            2) repeat is for one or more occurrence.
+               This is bit subtle. The correct number of times of 
+               deleting/ignoring/skipping will result in either case
+               being True.
+               e.g. s = 'aaa', p = 'a*a'
+                    skips twice
+                    s = 'a', p[2:] = 'a' -> True
+               e.g. s = 'aab', p = 'a*b'
+                    ...
+                    s = 'b', p = 'a*b'
+                    zero_ocr = isMatch('b', 'b') -> True
+                    first_char_match = False
+                    return zero_ocr, thus return True in the end
+            """
+            zero_ocr = self.isMatch(s, p[2:])
+            if first_char_match:
+                repeat = self.isMatch(s[1:], p)
+                return zero_ocr or repeat
+            return zero_ocr
+            # Simplified version
+            # return self.isMatch(s, p[2:]) \
+            #        or (first_char_match and self.isMatch(s[1:], p))
+        else:
+            if first_char_match:
+                return self.isMatch(s[1:], p[1:])
+            else:
+                return False
