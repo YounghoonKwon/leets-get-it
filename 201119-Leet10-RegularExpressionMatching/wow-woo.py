97dc08fb3db82fead2f8c3a38de976a92688f58b
wow-woo <51289355+wow-woo@users.noreply.github.com>
2020-11-19 16:53:17 +0900

    Try recursion
    - trying to implement solution with recursion

--- 201119-Leet10-RegularExpressionMatching/wow-woo.py
+++ 201119-Leet10-RegularExpressionMatching/wow-woo.py
@@ -1,51 +1,46 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        s_len = len(s)
-        p_len = len(p)
-        
-        #position on pattern
         p_idx = 0
         for i in range(len(s)):
-            print('index of i :', i, s[i])
             current = s[i]
+            print('current', s[i], i)
             
-            # pattern should fit length of s
-            if p_idx >= p_len:
-                print('#1',"length doesn't match")
+            p_idx = self.rcs(current, p_idx, p)
+            print('ss', p_idx)
+            if i == len(s)-1 and p_idx >= len(s):
+                return True
+            if p_idx < 0 or p_idx >= len(p):
                 return False
-            pattern = p[p_idx]
-            if pattern == '.':
-                pattern = current
-                
-            if pattern == '*':
-                # bring prior charactor in pattern
-                p_idx -= 1
-                pattern = p[p_idx]
-                print('now', pattern)
-                
-                if pattern == '.':
-                    pattern = current
-            if True:
-                # just compare charators
-                if current == pattern:
-                    print('#3', current, pattern)
-
-                    p_idx += 1
-                    
-                    continue
-                # no match
-                else:
-                    print('#4', current, pattern)
-
-                    #check if * exists right after the charactor in pattern 
-                    if p_idx+1 < p_len-1 and p[p_idx+1] == '*':
-                        if current != p[p_idx+2]:
-                            return False
-                        print('#5', current, pattern)
-                        p_idx += 3
-                        
-                        continue
-                    else:
-                        print('#6', current, pattern)
-                        return False
         return True
+    
+    def rcs(self, current, p_idx, p):
+        if p_idx >= len(p):
+            return -1
+        
+        pattern = p[p_idx]
+        print('pattern', pattern, p_idx)
+        # there is .
+        if pattern == '.':
+            return p_idx + 1
+        # there is *
+        elif pattern == '*':
+            # extend prior chractor with *
+            case1 = self.rcs(current, p_idx-1, p)
+            case2 = self.rcs(current, p_idx + 1, p)
+            return case1, case2
+        # there is only a letter
+        else:
+            # match
+            if current == pattern:
+                return p_idx+1
+            else:
+                # case of skip a charactor with * #compare current with charactor post *
+                if '*' == p[p_idx+1]:
+                    print('start to check whether skipping or wrong')
+                    print(p[p_idx], p[p_idx+1])
+                    print('result', self.rcs(current, p_idx+2, p))
+                    return self.rcs(current, p_idx+2, p)
+                else:
+                    # in order to return False
+                    return -1
+        
