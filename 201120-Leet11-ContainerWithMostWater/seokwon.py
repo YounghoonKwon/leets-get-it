4e7d8f2e1eb51cca5ed98d844bc1f3f69535e76e
seok10code <73082542+seok10code@users.noreply.github.com>
2020-11-20 23:43:23 +0900

    Create seokwon.py

---
+++ 201120-Leet11-ContainerWithMostWater/seokwon.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        water = 0
+        left = 0
+        right = len(height)-1
+        
+        while left<=right:
+            water = max(water, (right-left)*min(height[right],height[left]))
+
+            if height[left] < height[right]:
+                left +=1
+            else:
+                right -=1
+        return water
