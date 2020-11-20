392de24f7073874c30634f2a6d4df40e828de8f2
wow-woo <51289355+wow-woo@users.noreply.github.com>
2020-11-20 14:26:27 +0900

    zip( iterators )
    - If the passed iterators have different lengths, the iterator with the least items decides the length of the new iterator.

--- 201120-Leet14-LongestCommonPrefix/soungkook.py
+++ 201120-Leet14-LongestCommonPrefix/soungkook.py
@@ -1,24 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 0:
-            return ''
-        
-        prefix = ''
-        spot = 0
-        common = True
-        strs = sorted(strs, key=lambda item:len(item))
-        
-        while common:
-            if spot >= len(strs[0]):
+        len_ = 0
+        print('*', *strs)
+        print('zip', list(zip(*strs)))
+        for i in zip(*strs):
+            print('i', i)
+            if len(set(i)) == 1:
+                len_ += 1
+            else:
                 break
-            candidate = strs[0][spot]
-            for i in range(1, len(strs)):
-                if candidate != strs[i][spot]:
-                    common = False
-                    break
-            if common:
-                prefix += candidate
-                
-            spot += 1
-        
-        return prefix
+        return strs[0][:len_] if len_ > 0 else ''
