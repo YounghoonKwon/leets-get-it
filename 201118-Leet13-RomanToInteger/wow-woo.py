0fed654034bae5fe9ecbfbc8a29057b8a060b7e3
wow-woo <51289355+wow-woo@users.noreply.github.com>
2020-11-20 11:00:53 +0900

    Stack structure
    - push opening parentheses in through 
    - as you run into a Closing parenthese, Pop stack and compare with it

---
+++ 201118-Leet13-RomanToInteger/wow-woo.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for i in range(len(s)):
+            #Opening parentheses
+            if s[i] in ['[', '(', '{']:
+                stack.append(s[i])
+            #Closing parentheses
+            elif s[i] in [']',')','}']:
+                #absence means order is NOT valid or Not match
+                if len(stack) == 0:
+                    return False
+                #Bring latest item from stack
+                last =stack.pop()
+                
+                #Concatenate to simply comapre
+                current = last + s[i]
+                if current not in ['[]', '{}', '()']:
+                    return False
+            else:
+                continue
+                
+        # string is not formed pairs
+        if len(stack) != 0:
+            return False
+        return True            
