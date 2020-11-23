f0400487398de69b1dbe411e225cc3fdf1d13303
seok10code <73082542+seok10code@users.noreply.github.com>
2020-11-23 21:26:01 +0900

    Create seokwon.py

---
+++ 201123-Leet134-GasStation/seokwon.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost): 
+            return -1
+        
+        n, start, wallet = len(gas), 0, 0
+        for i in range(n):
+            wallet += gas[i] - cost[i]
+            if wallet < 0:
+                start, wallet = i+1, 0
+        return start
