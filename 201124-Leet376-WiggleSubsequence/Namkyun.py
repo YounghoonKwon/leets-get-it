d10bc65e9488ad690cc9de5dce205800b6ffb855
Namkyun Kim <55725244+Namkyun-AUX@users.noreply.github.com>
2020-11-24 22:10:33 +0900

    Update Namkyun.py

--- 201124-Leet376-WiggleSubsequence/Namkyun.py
+++ 201124-Leet376-WiggleSubsequence/Namkyun.py
@@ -9,7 +9,7 @@ class Solution(object):
             return 0
 
         up = down = 1
-        for i in xrange(1, len(nums)):
+        for i in range(1, len(nums)):
             if nums[i] > nums[i - 1]:
                 up = down + 1
             elif nums[i] < nums[i - 1]:
