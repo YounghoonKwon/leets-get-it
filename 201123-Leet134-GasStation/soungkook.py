19f11afc57a7d16e258cec1c7b3d8af506d9977c
wow-woo <51289355+wow-woo@users.noreply.github.com>
2020-11-24 14:25:45 +0900

    Using J index
    - Reduce extra variables
    - Meaningless performance improvement

--- 201123-Leet134-GasStation/soungkook.py
+++ 201123-Leet134-GasStation/soungkook.py
@@ -3,23 +3,20 @@ class Solution:
         #완주
         #clock wise
         for i in range(len(gas)):
-            current = i
             tank = 0
-            for j in range(len(gas)):
-                if i >= len(gas):
-                    tank += gas[i - len(gas)]
-                    tank = tank - cost[i - len(gas)]
-                else:
-                    tank += gas[i]
-                    tank = tank - cost[i]
+            for j in range(i, i + (len(gas))):
+                idx = j
                 
-                   
+                if j >= len(gas):
+                    idx = j - (len(gas))
+                
+                tank += gas[idx]
+                tank -= cost[idx]
+                
                 if tank < 0:
                     break
-                    
-                if j == len(gas)-1:
-                    return current
-                    
-                i += 1
+                
+                if j == i + len(gas) - 1:
+                    return i
             
         return -1
