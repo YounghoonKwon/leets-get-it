c984b204f34674afc2bccaca0540d1014f028975
younghoonkwon <younghoon.kwon91@gmail.com>
2020-11-25 09:43:34 +0900

    Create younghoon.py

---
+++ 201124-Leet406-QueueReconstructionByHeight/younghoon.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people = sorted(people, key = lambda x: (-x[0], x[1]))
+        res = []
+        for per in people:
+            res.insert(per[1], per)
+        return res
