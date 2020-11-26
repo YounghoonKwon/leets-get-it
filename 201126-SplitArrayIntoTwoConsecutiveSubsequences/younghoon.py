f885935af23ed4b9e0c3a1443384554242b6956a
younghoonkwon <younghoon.kwon91@gmail.com>
2020-11-26 17:32:29 +0900

    Create younghoon.py

---
+++ 201126-SplitArrayIntoTwoConsecutiveSubsequences/younghoon.py
@@ -0,0 +1,18 @@
+import collections
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        left = collections.Counter(nums)
+        end = collections.Counter()
+        for i in nums:
+            if not left[i]: continue
+            left[i] -= 1
+            if end[i - 1] > 0:
+                end[i - 1] -= 1
+                end[i] += 1
+            elif left[i + 1] and left[i + 2]:
+                left[i + 1] -= 1
+                left[i + 2] -= 1
+                end[i + 2] += 1
+            else:
+                return False
+        return True
