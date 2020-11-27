4003bcf3a4663acf5db3d06564f32d541bbc1613
wow-woo <51289355+wow-woo@users.noreply.github.com>
2020-11-27 18:11:07 +0900

    Create soungkook.py

---
+++ 201126-Leet714-BestTimeToBuyAndSellStockWithTransactionFee/soungkook.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        #   empty holding
+        dp = [ [0,0] for _ in range(len(prices))]
+        dp[0][0]=0
+        dp[0][1]=-prices[0]-fee
+        
+        for i in range(1,len(prices)):
+            dp[i][0], dp[i][1] = max(dp[i-1][0], dp[i-1][1] + prices[i]) , max(dp[i-1][0]-prices[i]-fee, dp[i-1][1])
+        print('dp',dp)
+        return dp[-1][0]
