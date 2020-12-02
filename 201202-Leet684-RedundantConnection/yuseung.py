fa54d326c6fec493445b6e2b3bb368b6b89676b5
kimys0279 <ykim49@ncsu.edu>
2020-12-02 22:38:30 +0900

    Create yuseung.py

---
+++ 201202-Leet684-RedundantConnection/yuseung.py
@@ -0,0 +1,22 @@
+  def findRedundantConnection(self, edges):
+    parent = [0] * len(edges)
+
+    def find(x):
+      if parent[x] == 0:
+        return x
+      parent[x] = find(parent[x])
+      return parent[x]
+    
+    def union(x, y):
+      rootX = find(x)
+      rootY = find(y)
+      if rootX == rootY:
+        return False
+      parent[rootX] = rootY
+      return True
+      
+    for x, y in edges:
+      if not union(x - 1, y - 1): 
+        return [x, y]
+      
+    raise ValueError("Illegal input.")
